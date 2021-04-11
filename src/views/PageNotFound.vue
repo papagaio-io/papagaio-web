4550cdfa29234ef6ac5548f8502362713317f9ce
Mohab <mohababugabal@gmail.com>
2021-04-11 22:49:50 +0200

    restoring2

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -2,11 +2,7 @@
   
     <div class="flex h-screen">
   <div class="m-auto text-3xl font-extrabold tracking-tight sm:text-4xl">
-<<<<<<< HEAD
-404 Error: Page not found
-=======
-404: Page not found error
->>>>>>> 080a23ffab1b6da42bf9fc819824ba0cda96a23b
+404 error: Page not found error
   </div>
     </div>
 </template>
