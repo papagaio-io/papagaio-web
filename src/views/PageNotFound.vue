fa5b77422852f31c4ee71b1bc53908f83b888f9e
Mohab <mohababugabal@gmail.com>
2021-04-12 10:33:01 +0200

    not found message

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -2,7 +2,7 @@
   
     <div class="flex h-screen">
   <div class="m-auto text-3xl font-extrabold tracking-tight sm:text-4xl">
-404 error: Page not found error
+404 error: Page not found error. Please do not come here again.
   </div>
     </div>
 </template>
